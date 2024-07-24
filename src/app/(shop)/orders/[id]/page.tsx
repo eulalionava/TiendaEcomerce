2906183d769a1968208548d7798184bc4f7245cb
eulalio <eulalio@readymind.ms>
2024-07-23 23:44:45 -0600

    agregando rutas

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function() {
+  return (
+    <div>
+      <h1>Orden page</h1>
+    </div>
+  );
+}
---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
